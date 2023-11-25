7d5eb7e55ec31ba7c1e39b596ce2fc5a86730e55
Anton Pashkevich <natoy7000@gmail.com>
2023-11-25 12:30:35 +0100

    Add Quaternion

---
+++ Assets/Quaternion/RotateToTargetQuaternion.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Quaternion
+{
+    public class RotateToTargetQuaternion : MonoBehaviour
+    {
+        [SerializeField] private float rotationSpeed = 1;
+        [SerializeField] private Transform _transformTarget;
+        [SerializeField] private Transform _transformCommunicate;
+
+        private void OnDrawGizmos() {
+            RotateToTarget();
+            Handles.Label(_transformCommunicate.position, gameObject.name);
+            Debug.DrawLine(transform.position, transform.position + transform.forward * 3f, Color.blue);
+        }
+
+
+        private void RotateToTarget() {
+            if (_transformTarget != null)
+            {
+                if (_transformTarget != null)
+                {
+                    Vector3 directionToTarget = _transformTarget.position - transform.position;
+                    UnityEngine.Quaternion targetRotation = UnityEngine.Quaternion.LookRotation(directionToTarget, Vector3.up);
+                    transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed);
+                }
+            }
+        }
+    }
+}
